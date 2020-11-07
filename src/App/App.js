f41d62abc5a00973cc5702a7fb78e7749f824ff8
Hanna Kim <hannakim91@gmail.com>
2020-11-06 17:06:38 -0700

    Set initial state for App

--- src/App/App.js
+++ src/App/App.js
@@ -2,9 +2,10 @@ import logo from '../location.png';
 import './App.css';
 import Cities from '../Cities/Cities.js';
 import { getAllCities } from '../apiCalls.js';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const App = () => {
+  const [cities, setCities] = useState({ cities: [] })
   return (
     <section className="App">
       <header className="App-header">
