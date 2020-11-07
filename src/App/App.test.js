21c65e4ef2743bb436df8168134e1561beda64e1
Hanna Kim <hannakim91@gmail.com>
2020-11-07 15:18:20 -0700

    Test async data fetch for App

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -4,13 +4,28 @@ import "@testing-library/jest-dom";
 import { getAllCities } from '../apiCalls.js'
 import '@testing-library/jest-dom/extend-expect';
 import App from './App';
+jest.mock('../apiCalls.js');
 
 describe('App', () => {
-
-  it('renders the App', () => {
+  beforeEach(() => {
+    getAllCities.mockResolvedValue({
+      _links: {
+        ['ua:item']: [
+          {name:"Atlanta"}, 
+          {name: "Bali"},
+          {name: "Caracas"}
+        ]
+      }
+    })
+  })
+  it('renders the App', async () => {
     render(<App />);
     const header = screen.getByText("Settlr");
     expect(header).toBeInTheDocument();
+    await waitFor(() => expect(screen.getByText("Atlanta")).toBeInTheDocument())
+    expect(screen.getByText("Bali")).toBeInTheDocument()
+    expect(screen.getByText("Caracas")).toBeInTheDocument()
+
   });
 
 })
