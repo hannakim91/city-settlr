366bc0edcb7da2cbd6bc378bbfbee29a8371eb18
Hanna Kim <hannakim91@gmail.com>
2020-11-07 14:15:39 -0700

    Install testing libraries and import to App test

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,4 +1,8 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import "@testing-library/jest-dom";
+import { getAllCities } from '../apiCalls.js'
+import '@testing-library/jest-dom/extend-expect';
 import App from './App';
 
 describe('App', () => {
