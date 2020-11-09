a4cf6b6a52ec4cf0ff49e41505518d45bcaebccc
Hanna Kim <hannakim91@gmail.com>
2020-11-09 15:25:25 -0700

    Set initial state for city details

--- src/DetailsPage/DetailsPage.js
+++ src/DetailsPage/DetailsPage.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { getSingleCity } from '../apiCalls.js'
 
 const DetailsPage = ({city}) => {
+  const [details, setDetails] = useState({})
   return (
     <>
      <h2>{city}</h2>
