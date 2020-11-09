3cb8484d147001039664ed6d3c90e10f67cb3992
Hanna Kim <hannakim91@gmail.com>
2020-11-09 10:45:32 -0700

    Delete unused code in CityCard

--- src/CityCard/CityCard.js
+++ src/CityCard/CityCard.js
@@ -1,18 +1,11 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import defaultScale from '../assets/scale-bw.png'
 import colorScale from '../assets/scale-color.png'
 
 const CityCard = ({id, name, toggleCompareList, selected}) => {
-  // console.log(selected)
-  // const [toggle, setToggle] = useState(selected)
-  
-  //toggle only happening on click after going from compare page to main page 
-  //is my name in the list? if so - be selected instead of toggle?
-  const toggleCompare = () => {
-    toggleCompareList(name)
-    // compareList.includes(name) ? setToggle(false) : setToggle(true)
-  }
+
   let compareButton = selected ? colorScale : defaultScale
+
   return (
     <section id={`city-${id}`}>
       <img 
@@ -26,18 +19,3 @@ const CityCard = ({id, name, toggleCompareList, selected}) => {
 }
 
 export default CityCard
-
-//imgs for compare off / compare on
-//
-
- // useEffect(() => {
-  //   const clickHandler = ( { target } ) => {
-  //     console.log('target', target.id)
-  //     const container = document.getElementById(`city-${id}`)
-  //     console.log(container)
-  //     if (container.id === target.id) return;
-  //     setToggle(false)
-  //   }
-  //   document.addEventListener('click', clickHandler)
-  //   return () => document.removeEventListener('click', clickHandler)
-  // })
