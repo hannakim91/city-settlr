e94ffb3eaf4066d806daff0487c282952f56dc9c
Hanna Kim <hannakim91@gmail.com>
2020-11-09 15:46:46 -0700

    Render quality of life categories &  ratings

--- src/DetailsPage/DetailsPage.js
+++ src/DetailsPage/DetailsPage.js
@@ -15,10 +15,15 @@ const DetailsPage = ({city}) => {
     }
     fetchDetails()
   }, [city])
+
+  // const categoryList = details.categories.map(category => <li>category.name</li>)
   return (
     <>
      <h2>{city}</h2>
      {details.summary && details.summary.replace( /(<([^>]+)>)/ig, '')}
+     <ul>
+       {details.categories && details.categories.map(category => <li>{category.name}: {Math.round(category.score_out_of_10 * 10) / 10}</li>)}
+     </ul>
     </>
   )
 }
