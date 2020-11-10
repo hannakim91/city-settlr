9049a0febd849539dcbe70a3d0563296e15462ff
Hanna Kim <hannakim91@gmail.com>
2020-11-09 20:34:47 -0700

    Style ComparePage

--- src/ComparePage/ComparePage.js
+++ src/ComparePage/ComparePage.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { getComparisonData } from '../apiCalls.js'
 import PropTypes from 'prop-types'
+import './ComparePage.css'
 
 const ComparePage = ({compareList}) => {
   const [comparisonData, setComparisonData] = useState({citiesToCompare: []})
@@ -23,19 +24,21 @@ const ComparePage = ({compareList}) => {
   }
 
   return (
-    <>
+    <article className="ComparePage">
       <h2>Compare Cities</h2>
-      {comparisonData.length && comparisonData.map((city, i) => (
-        <section>
-          <h3>{compareList[i]}</h3>
-          <p>{city.categories[4].name}: {Math.round(city.categories[4].score_out_of_10 * 10) / 10}</p>
-          <p>{city.categories[13].name}: {Math.round(city.categories[13].score_out_of_10 * 10) / 10}</p>
-          <p>{city.categories[11].name}: {Math.round(city.categories[11].score_out_of_10 * 10) / 10}</p>
-          <p>{city.categories[14].name}: {Math.round(city.categories[14].score_out_of_10 * 10) / 10}</p>
-          <p>Egalitarianism: {Math.round(city.categories[14].score_out_of_10 * 10) / 10}</p>
-        </section>
-      ))}
-    </>
+      <section className="compare-section">
+        {comparisonData.length && comparisonData.map((city, i) => (
+          <article class="city-stats">
+            <h3>{compareList[i]}</h3>
+            <p>{city.categories[4].name}: {Math.round(city.categories[4].score_out_of_10 * 10) / 10}</p>
+            <p>{city.categories[13].name}: {Math.round(city.categories[13].score_out_of_10 * 10) / 10}</p>
+            <p>{city.categories[11].name}: {Math.round(city.categories[11].score_out_of_10 * 10) / 10}</p>
+            <p>{city.categories[14].name}: {Math.round(city.categories[14].score_out_of_10 * 10) / 10}</p>
+            <p>Egalitarianism: {Math.round(city.categories[14].score_out_of_10 * 10) / 10}</p>
+          </article>
+        ))}
+      </section>
+    </article>
   )
 }
 
